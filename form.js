e2c75b87a2bd963248ef62db9899d05cec10936c
suhelparkar <suhelparkar2013@gmail.com>
2016-03-23 17:21:02 +0400

    Added files via upload

--- form.js
+++ form.js
@@ -1,14 +1,4 @@
-var today = new Date();
-function calcAge(birthDate, givenDate) {
-    givenDate = new Date(today);
-    var dt1 = document.getElementById('dob').value;
-    var birthDate = new Date(dt1);
-    var age = (givenDate.getFullYear() - birthDate.getFullYear());
-    return age;
-}
-
-function regForm() {
-
+var validateRegistrationForm = function () {
     var name = /^[A-Za-z]+$/;
     var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var age = calcAge(age);
@@ -17,40 +7,38 @@ function regForm() {
         alert('Name field must have alphabet characters only');
         return false;
     }
-    else if(!mail.value.match(email)) {
+    else if (!mail.value.match(email)) {
         alert('Invalid Email Entered');
         return false;
     }
-    else if(age<0) {
+    else if (age < 0) {
         alert('Invalid Date of Birth Entered');
         return false;
     }
-    else{
-        return reg();
+    else {
+        var registerForm = function () {
+            var gender = document.form.gender.value;
+            var int = document.form.interest.value;
+            var email = document.form.mail.value;
+            if (gender == "male") {
+                alert("Hello, Sir! You are " + age + " years old. We will send you mails on your selected topic of interest : " + int + " on your email id : " + email + ".");
+            } else if (gender == "female") {
+                alert("Hello, Madam! You are " + age + " years old. We will send you mails on your selected topic of interest : " + int + " on your email id : " + email + ".");
+            } else {
+                alert("Hello! You are " + age + " years old. We will send you mails on your selected topic of interest : " + int + " on your email id : " + email + ".");
+            }
+        };
+        return registerForm();
     }
+    function calcAge() {
+        var today = new Date();
+        var userdt = document.getElementById('dob').value;
+        var birthDate = new Date(userdt);
+        var age = (today.getFullYear() - birthDate.getFullYear());
+        return age;
 
-}
-
-
-function reg() {
-
-    var first = document.form.fn.value;
-    var last = document.form.ln.value;
-    var email = document.form.mail.value;
-    var gender = document.form.gender.value;
-    var int = document.form.interest.value;
-    var age = calcAge(age);
-
-    //debugger;
-    if (gender == "male") {
-        alert("Hello, Sir! You are "+age+" years old. We will send you mails on your selected topic of interest : "+int+" on your email id : "+email+".");
-    } else if (gender == "female") {
-        alert("Hello, Madam! You are "+age+" years old. We will send you mails on your selected topic of interest : "+int+" on your email id : "+email+".");
-    } else  {
-        alert("Hello! You are "+age+" years old. We will send you mails on your selected topic of interest : "+int+" on your email id : "+email+".");
-    // }else {
-    //     alert("Hello! We will send you mails on your selected topic of interest : "+int+" on your email id : "+email+".");
-    }
+    };
+};
 
 
-}
+
