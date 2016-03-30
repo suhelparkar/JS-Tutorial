30691b72d868c904b1202eb1ac4d0fd46dfbcf70
suhelrf <suhel@realforce.ch>
2016-03-30 13:24:44 +0400

    person form

--- form.js
+++ form.js
@@ -1,98 +1,43 @@
-var validateRegistrationForm = function () {
-
+var personRegistrationForm = function () {
+    var FirstName = document.getElementById("firstName").value;
+    var LastName = document.getElementById("lastName").value;
+    var Email = document.getElementById("eMail").value;
+    var DOB = document.getElementById("age").value;
+    var AGE = calcAge();
     var validateName = /^[A-Za-z]+$/;
     var validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var age = calcAge(age);
-    var FirstName = document.form.fname;
-    var LastName = document.form.lname;
-    var Email = document.form.email;
-    var Age = document.form.age;
-    var Interest = document.getElementsByName('interest')[0];
-
-    if (FirstName.value == "" )
-    {
-        window.alert("Please enter your first name.");
-        FirstName.focus();
-        return false;
+    if (document.getElementById("m").checked) {
+        var Gender = document.getElementById("m").value;
+    } else if (document.getElementById("f").checked) {
+        var Gender = document.getElementById("f").value;
     }
+    var Interest = document.getElementById("int").value;
 
-    else if (!FirstName.value.match(validateName))
-    {
-        window.alert("Please enter valid first name.");
-        FirstName.focus();
-        return false;
-    }
-
-    else if (LastName.value == "")
-    {
-        window.alert("Please enter your last name.");
-        LastName.focus();
-        return false;
-    }
-
-    else if (!LastName.value.match(validateName))
-    {
-        window.alert("Please enter valid last name.");
-        LastName.focus();
-        return false;
-    }
-
-    else if (Email.value == "")
-    {
-        window.alert("Please enter e-mail address.");
-        Email.focus();
-        return false;
-    }
-
-    else if (!Email.value.match(validateEmail))
-    {
-        window.alert("Please enter valid e-mail address.");
-        Email.focus();
-        return false;
-    }
-
-    else if (Age.value == "")
-    {
-        window.alert("Please enter your Age");
-        Age.focus();
-        return false;
-    }
-
-    else if (age < 0) {
-        window.alert('Invalid Date of Birth Entered');
-        Age.focus();
-        return false;
-    }
-
-    else if (Interest.value== "") {
-        window.alert('Select your Interest');
-        Interest.focus();
-        return false;
-    }
-
-    else {
-        var registerForm = function () {
-            var gender = document.form.gender.value;
-            var choices = multipleInterest(choices);
-            var email = document.form.mail.value;
-            if (gender == "male") {
-                alert("Hello, Sir! You are " + age + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + email + ".");
-            } else if (gender == "female") {
-                alert("Hello, Madam! You are " + age + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + email + ".");
-            } else {
-                alert("Hello! You are " + age + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + email + ".");
-            }
-        };
-        return registerForm();
+    function registerForm() {
+        var AGE = calcAge();
+        var choices = multipleInterest();
+        var Email = document.getElementById("eMail").value;
+        if (document.getElementById("m").checked) {
+            var Gender = document.getElementById("m").value;
+        } else if (document.getElementById("f").checked) {
+            var Gender = document.getElementById("f").value;
+        }
+        if (Gender == "male") {
+            alert("Hello, Sir! You are " + AGE + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + Email + ".");
+        } else if (Gender == "female") {
+            alert("Hello, Madam! You are " + AGE + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + Email + ".");
+        } else {
+            alert("Hello! You are " + AGE + " years old. We will send you mails on your selected topic of interest : " + choices + " on your email id : " + Email + ".");
+        }
     }
 
     function calcAge() {
         var today = new Date();
-        var userdt = document.getElementById('dob').value;
+        var userdt = document.getElementById('age').value;
         var birthDate = new Date(userdt);
-        var age = (today.getFullYear() - birthDate.getFullYear());
-        return age;
-    };
+        var AGE = (today.getFullYear() - birthDate.getFullYear());
+        return AGE;
+    }
 
     function multipleInterest() {
         var choices = " ", i;
@@ -102,5 +47,121 @@ var validateRegistrationForm = function () {
             }
         }
         return choices;
-    };
-};
+    }
+
+    validateRegistrationForm();
+
+    function displayTable() {
+        var list = multipleInterest();
+        var personInterest = list;
+        var table = document.getElementById("DataTable");
+        var getRow = document.getElementById("getRow").value;
+        if (table.rows.length != getRow) {
+            table.deleteRow(getRow);
+        } else {
+            document.getElementById("getRow").value = getRow + 1;
+        }
+        var row = table.insertRow(getRow);
+        var cell1 = row.insertCell();
+        var cell2 = row.insertCell();
+        var cell3 = row.insertCell();
+        var cell4 = row.insertCell();
+        var cell5 = row.insertCell();
+        var cell6 = row.insertCell();
+        var cell7 = row.insertCell();
+        cell1.innerHTML = FirstName;
+        cell2.innerHTML = LastName;
+        cell3.innerHTML = Email;
+        cell4.innerHTML = DOB;
+        cell5.innerHTML = Gender;
+        cell6.innerHTML = personInterest;
+        var update = "Update";
+        var remove = "Remove";
+        var action = '<a href="#" onclick="updateTable(' + (getRow) + ');"  > Update</a>';
+        cell7.innerHTML = action;
+        resetForm();
+    }
+
+    function validateRegistrationForm() {
+        if (FirstName == "") {
+            window.alert("Please enter your first name.");
+            firstName.focus();
+            return false;
+        }
+        else if (!FirstName.match(validateName)) {
+            window.alert("Please enter valid first name.");
+            firstName.focus();
+            return false;
+        }
+        else if (LastName == "") {
+            window.alert("Please enter your last name.");
+            lastName.focus();
+            return false;
+        }
+        else if (!LastName.match(validateName)) {
+            window.alert("Please enter valid last name.");
+            lastName.focus();
+            return false;
+        }
+        else if (Email == "") {
+            window.alert("Please enter e-mail address.");
+            eMail.focus();
+            return false;
+        }
+        else if (!Email.match(validateEmail)) {
+            window.alert("Please enter valid e-mail address.");
+            eMail.focus();
+            return false;
+        }
+        else if (DOB == "") {
+            window.alert("Please enter your Age");
+            age.focus();
+            return false;
+        }
+        else if (AGE < 0) {
+            window.alert('Invalid Date of Birth Entered');
+            age.focus();
+            return false;
+        }
+        else if (Interest == "") {
+            window.alert('Select your Interest');
+            int.focus();
+            return false;
+        }
+        else {
+            registerForm();
+            displayTable();
+            updateTable();
+        }
+    }
+}
+function updateTable(tableRow) {
+    document.getElementById("firstName").value = DataTable.rows[tableRow].cells[0].innerHTML;
+    document.getElementById("lastName").value = DataTable.rows[tableRow].cells[1].innerHTML;
+    document.getElementById("eMail").value = DataTable.rows[tableRow].cells[2].innerHTML;
+    document.getElementById("age").value = DataTable.rows[tableRow].cells[3].innerHTML;
+    if (DataTable.rows[tableRow].cells[4].innerHTML == "male") {
+        document.getElementById("m").checked = true;
+    } else {
+        document.getElementById("f").checked = true;
+    }
+    for (var i = 0; i < document.getElementById("int").length; i++) {
+        if (document.getElementById("int")[i].value == DataTable.rows[tableRow].cells[5].innerHTML) {
+            document.getElementById("int").selectedIndex = i;
+            break;
+        }
+    }
+    document.getElementById("getRow").value = tableRow;
+    document.getElementById("formUpdate").value = "Update";
+}
+function resetForm() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("eMail").value = "";
+    document.getElementById("m").checked = false;
+    document.getElementById("f").checked = false;
+    document.getElementById("age").value = "";
+    document.getElementById("int").selectedIndex = 0;
+    document.getElementById("formUpdate").value = "Add";
+    document.getElementById("getRow").value = document.getElementById("DataTable").rows.length;
+}
